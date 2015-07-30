c7723f6a23c163378a7e00f5fa3ec2518b7a7c42
larkos <larkos@hotmail.se>
2015-07-30 22:46:46 +0200

    First example check in

---
+++ DreamOfLiving_Spaces_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DreamOfLiving_Spaces_Web.Startup))]
+namespace DreamOfLiving_Spaces_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
